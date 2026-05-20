2b902af6e0a4ad9dc5b6b0ef7dccd4313ba39aff
Shino <100228440+LargyThomas@users.noreply.github.com>
2026-05-20 10:51:38 +0200

    feat(routes): add new route with the new security functions #43

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./features/auth/auth.routes.js');
+const verifierToken = require('./security/middleware/auth.middleware');
+const autoriserRole = require('./security/middleware/roles.middleware');
 
 dotenv.config();
 
@@ -17,6 +19,11 @@ app.use('/api/auth', authRoutes);
 // Health check endpoint
 app.get('/api/health', (req, res) => {
     res.status(200).send("Ok");
+});
+
+// Example of a protected route with role-based access control
+app.get('/admin', verifierToken, autoriserRole(['admin']), (req, res) => {
+    res.json({ message: "Bienvenue dans la section admin" });
 });
 
 module.exports = app;
