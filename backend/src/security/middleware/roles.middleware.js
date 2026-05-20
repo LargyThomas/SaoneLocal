c66b34cce799d89e0a0177f43f5ef6fc65f465f4
Shino <100228440+LargyThomas@users.noreply.github.com>
2026-05-20 10:50:26 +0200

    feat(security): add permissions based on user roles #43

--- backend/src/security/middleware/roles.middleware.js
+++ backend/src/security/middleware/roles.middleware.js
@@ -1,1 +1,15 @@
 // Controls permissions based on user roles (RBAC)
+
+const rolesMiddleware = (requireRoles) => {
+    return(req, res, next) => {
+        if (!req.user){
+            return res.status(401).json({ message: "Rôle manquant." });
+        }
+        if (!requireRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Accés refusé. Rôle insuffisant." });
+        }
+        next();
+    }
+}
+
+module.exports = rolesMiddleware;
