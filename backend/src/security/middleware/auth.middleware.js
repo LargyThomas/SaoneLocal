5e06d20700d831ee8bbdb7c5985f99552ab4b593
Shino <100228440+LargyThomas@users.noreply.github.com>
2026-05-20 10:49:23 +0200

    feat(security): add jwt securisation with token verifcation #43

--- backend/src/security/middleware/auth.middleware.js
+++ backend/src/security/middleware/auth.middleware.js
@@ -1,1 +1,22 @@
 // Verifies JWT token and protects routes
+
+const { verifyToken } = require('../jwt.js');
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Without Bearer
+
+    if (!token) {
+        return res.status(401).json({ message: "Token d'authentification manquant."});
+    }
+    
+    try {
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Token d'authentification invalide ou expiré." });
+    }
+}
+
+module.exports = authMiddleware;
