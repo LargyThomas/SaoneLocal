36d86a753200cb45390b27173ff7964dd1718f66
Chenevoix Clemence <cchenevoix@gmail.com>
2026-06-03 14:20:43 +0200

    docs(producer_routes): add comments to the functions

--- backend/src/features/producers/producers.validation.js
+++ backend/src/features/producers/producers.validation.js
@@ -1,11 +1,15 @@
-// producers.validation.js\n// TODO: implement producers validation logic\n\nmodule.exports = {};\n
-
+/**
+* @description Check if the id given is a number
+* @param {hash} req, the request
+* @param {hash} res, the response of the request
+* @param {Function: next} next, the next function
+* @return {Function: next} or {Error} the next function or an error if the id is not valid
+*/
 const validationId = (req, res, next) => {
     if (isNaN(parseInt(req.params.id))) {
         return res.status(400).json({ message: "L'Id du producteur doit être un nombre" });
     }
     next();
 }
-//regarder si c'est une id
 
 module.exports = {validationId}
