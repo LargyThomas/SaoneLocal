c15e8d36b1f5c34bcc44848e11137b90765bff99
Shino <100228440+LargyThomas@users.noreply.github.com>
2026-06-04 14:38:55 +0200

    feat(route): add the favorite route in the app.js #49

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 // Routes
 const authRoutes = require('./features/auth/auth.routes.js');
 const producerRoutes = require('./features/producers/producers.routes.js')
+const favoritesRoutes = require('./features/favorites/favorites.routes.js');
 
 // Middlewares
 const injectionMiddleware = require('./security/middleware/injection.middleware.js');
@@ -27,16 +28,18 @@ app.use(injectionMiddleware);
 app.use('/api/auth', authRoutes);
 
 // Producer routes
-
 app.use('/api/producer', producerRoutes);
 
+// Favorites routes
+app.use('/api/favorites', favoritesRoutes);
+
 // Test routes
-app.get('/api/test/protected', authMiddleware, (req, res) => {
+app.get('/api/test/order', authMiddleware, (req, res) => {
     res.status(200).json({ message: 'Accès autorisé', user: req.user });
 });
 
-app.get('/api/test/admin-only', authMiddleware, rolesMiddleware(['admin']), (req, res) => {
-    res.status(200).json({ message: 'Accès admin autorisé' });
+app.get('/api/test/order', authMiddleware, rolesMiddleware(['producer']), (req, res) => {
+    res.status(200).json({ message: 'Accès producer autorisé' });
 });
 
 // Error handler
